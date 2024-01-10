87db50b2c235fb461c7b0641df27fb8d46147048
Artem Levchenko <evgn.main@gmail.com>
2024-01-10 16:34:38 +0200

    Class Human

---
+++ Sana06/ClassLibrary1/Human.cs
@@ -0,0 +1,29 @@
+﻿namespace ClassLibrary1;
+
+public class Human
+{
+    private string _firstName;
+    private string _lastName;
+    private DateTime _birthDate;
+
+    public Human()
+    {
+        
+    }
+
+    public Human(string firstName, string lastName, DateTime birthDate)
+    {
+        _firstName = firstName;
+        _lastName = lastName;
+        _birthDate = birthDate;
+    }
+    
+    public virtual void ShowInfo()
+    {
+        Console.Write($"{_firstName} {_lastName} {_birthDate.ToShortDateString()} ");
+    }
+    
+    public string FirstName => _firstName;
+    public string LastName => _lastName;
+    public DateTime BirthDate => _birthDate;
+}
